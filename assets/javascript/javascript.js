2e6b11304ab956c8f43ba0fe34f4d9f0dd15410b
xiongkb <mkan-bx@hotmail.com>
2020-01-25 21:28:38 -0800

    format files to look better and added styling to restaurant css

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // global varibles to be used
     var queryURL;
     var ingredArray = [];
-    var tempPlaceID ="";
+    var tempPlaceID = "";
 
-
-
-    $("#addButton").on("click",function(event){
+    $("#addButton").on("click", function (event) {
         event.preventDefault();
         userSearch = $("#ingredient").val();
         if (userSearch.search(/[^a-zA-Z]+/) > -1) {
@@ -20,22 +18,21 @@ $(document).ready(function(){
             ingredArray.push(userSearch);
         }
         $(".addedIngredients").text(ingredArray);
-        
+
     })
     // Button to clear
-    $("#clearButton").on("click",function(event){
+    $("#clearButton").on("click", function (event) {
         event.preventDefault();
         userSearch = $("#ingredient").val();
         ingredArray = [];
         $(".addedIngredients").text(ingredArray);
-        
+
     })
     // button to submit
-    $("#submitButton").on("click",function(event){
+    $("#submitButton").on("click", function (event) {
         event.preventDefault();
         tempStr = ingredArray.toString();
-        tempStr = tempStr.replace(",","+");
-        // console.log(tempStr)
+        tempStr = tempStr.replace(",", "+");
         appId = "e7e14c99";
         appKey = "8b305785d6e489018ccfd57f33064460";
         queryURL = `https://api.edamam.com/search?q=${tempStr}&app_id=${appId}&app_key=${appKey}`;
@@ -43,62 +40,49 @@ $(document).ready(function(){
     });
 
     // Create function for buttons regarding cuisine
-    $(".cuisineButton").on("click",function(event){
-        console.log(this);
+    $(".cuisineButton").on("click", function (event) {
         buttonPressed = $(this).val();
-        apiKey = "";
+        apiKey = ""; //enter your google apiKey here
         queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${buttonPressed}&inputtype=textquery&type=restaurant&key=${apiKey}`
-        // console.log($(this).val());
         restaurantAPICall(queryURL);
-    
-        // secondqueryURL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${tempPlaceID}&fields=adr_address,formatted_address,geometry,icon,name,permanently_closed,photo,plus_code,type,url,utc_offset,vicinity&key=${apiKey}`
-        // setTimeout(async function(){
-        //     await anotherFunction
-        // },1000);
     });
 
-    var anotherFunction = function(param){
+    var anotherFunction = function (param) {
         secondqueryURL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${param}&fields=adr_address,formatted_address,geometry,icon,name,permanently_closed,photo,plus_code,type,url,utc_offset,vicinity&key=${apiKey}`
         placeDetailCall(secondqueryURL);
-        console.log(secondqueryURL)
     }
 
     // 2nd API function for restaurants
-    var restaurantAPICall =  async function(queryURL){
+    var restaurantAPICall = async function (queryURL) {
         var settings = {
             "url": queryURL,
             "method": "GET",
         }
 
-          await  $.ajax(settings).done(function (response) {
-            console.log(response);
+        await $.ajax(settings).done(function (response) {
             tempPlaceID = response.candidates[0].place_id;
 
         });
-        
         anotherFunction(tempPlaceID)
-        
     }
 
-    var placeDetailCall = function(secondqueryURL){
+    var placeDetailCall = function (secondqueryURL) {
         var settings = {
             "url": secondqueryURL,
             "method": "GET",
         }
         $.ajax(settings).done(function (response) {
-            console.log(response);
-            // Add response code here
             name = response.result.name;
             address = response.result.formatted_address;
             mapURL = response.result.url;
 
             newDiv = $("<div>");
             newDiv.html(`
-                <h2>${name}</h2>
+                <h3>${name}</h3>
                 <br>
-                <h3>${address}</h3>
+                <h4>${address}</h4>
                 <br>
-                <h3><a href=${mapURL}>Check out the location!</a></h3>
+                <h4><a href=${mapURL}>Check out the location!</a></h4>
                 <br>
             `);
             $(".results").html(newDiv);
@@ -106,13 +90,12 @@ $(document).ready(function(){
         });
     }
 
-    var makeAPICall = function(queryURL){
-    // api call for recipes based on ingredients
+    var makeAPICall = function (queryURL) {
+        // api call for recipes based on ingredients
         var settings = {
             "url": queryURL,
             "method": "GET",
         }
-    // https://api.edamam.com/search?q=chicken+apple+pear&app_id=e7e14c99&app_key=8b305785d6e489018ccfd57f33064460
 
         $.ajax(settings).done(function (response) {
             var recipeNums = [];
@@ -123,11 +106,11 @@ $(document).ready(function(){
             function displayFood() {
                 for (var i = 0; i < 3; i++) {
                     var rNum = -1;
-                    
-                    while(rNum === -1) {
+
+                    while (rNum === -1) {
                         var tempNum = Math.floor(Math.random() * 9);
 
-                        if(!recipeNums.includes(tempNum)) {
+                        if (!recipeNums.includes(tempNum)) {
                             rNum = tempNum;
                             recipeNums.push(tempNum);
                         }
@@ -150,12 +133,8 @@ $(document).ready(function(){
                     newDiv.append(h4);
                     newDiv.append(image);
                     $(".results").prepend(newDiv);
-
-                    console.log(rNum);
-                    console.log(response);
                 };
             }
-            
         });
-    }      
+    }
 });
